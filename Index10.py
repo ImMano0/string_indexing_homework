4af68c13bd97b2c17fe174fd504e23948c6fb7aa
ImMano0 <UrMa1371@gmail.com>
2023-07-06 19:40:28 +0500

    code

--- Index10.py
+++ Index10.py
@@ -6,4 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+ d = int(s)
+
+    d1  = d % 10
+    d //= 10
+
+    d2  = d % 10
+    d //= 10
+
+    d3  = d % 10
+    d //= 10
+
+    d4  = d % 10
+    d //= 10
+
+    d5  = d % 10
+    d //= 10 
+
+    return d1 + d2 + d3 + d4 + d5
+print(main('12305'))
